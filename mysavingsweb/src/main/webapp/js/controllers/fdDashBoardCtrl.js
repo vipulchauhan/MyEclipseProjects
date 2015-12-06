6e91e706daa2f47605dd7156f3ce4270dcf0fd7d
Vipul <08bce016@gmail.com>
2015-12-06 22:52:22 +0530

    updates

--- mysavingsweb/src/main/webapp/js/controllers/fdDashBoardCtrl.js
+++ mysavingsweb/src/main/webapp/js/controllers/fdDashBoardCtrl.js
@@ -7,79 +7,80 @@
 
 angular.module('app.controllers').controller(
 		'fdDashBoardCtrl',
-		function($scope, $http, $timeout) {
+		[
+				'$scope',
+				'$http',
+				'$timeout',
+				'fdCRUDService',
+				function($scope, $http, $timeout, fdCRUDService) {
 
-			$scope.baseURL = "http://localhost:8080";
-			//$scope.baseURL = "http://localhost:8081";
-			//$scope.appContextURL = $scope.baseURL + "/mySavings-service/mySavings";
-			$scope.appContextURL = $scope.baseURL + "/mySavings";
-			$scope.getVixedDepositesURL = $scope.appContextURL
-					+ "/fixedDeposites";
+					$scope.baseURL = "http://localhost:8080";
+					// $scope.baseURL = "http://localhost:8081";
+					// $scope.appContextURL = $scope.baseURL +
+					// "/mySavings-service/mySavings";
+					$scope.appContextURL = $scope.baseURL + "/mySavings";
+					$scope.getFixedDepositesURL = $scope.appContextURL
+							+ "/fixedDeposites";
 
-			$scope.fixedDepositeArray = [];
+					$scope.fixedDepositeArray = [];
 
-			// load engine series from server
-			$scope.loadFixedDeposites = function() {
-				console.log('loading Fixed Deposites');
-				$http.get($scope.getVixedDepositesURL)
-				.success(function(data) {
-					console.log('Fiexd Deposite data from Server :-');
-					console.log(JSON.stringify(data));
-					$scope.fixedDepositeArray = data;
-				})
-				
-			}
+					// load engine series from server
+					$scope.loadFixedDeposites = function() {
+						fdCRUDService.list($scope.getFixedDepositesURL);
+					}
 
-			$scope.tableDrawEnded = function() {
-				$timeout(function() {
-					$(document).ready(function() {
-						$('#fixedDepositeTable').DataTable();
-					});
+					$scope.tableDrawEnded = function() {
+						$timeout(function() {
+							$(document).ready(function() {
+								$('#fixedDepositeTable').DataTable();
+							});
 
-					$('[data-toggle="popover"]').popover();
+							$('[data-toggle="popover"]').popover();
 
-				}, 0);
-			}
-			$scope.opts = {
-				lines : 13 // The number of lines to draw
-				,
-				length : 0 // The length of each line
-				,
-				width : 14 // The line thickness
-				,
-				radius : 42 // The radius of the inner circle
-				,
-				scale : 1 // Scales overall size of the spinner
-				,
-				corners : 1 // Corner roundness (0..1)
-				,
-				color : '#000' // #rgb or #rrggbb or array of colors
-				,
-				opacity : 0.25 // Opacity of the lines
-				,
-				rotate : 0 // The rotation offset
-				,
-				direction : 1 // 1: clockwise, -1: counterclockwise
-				,
-				speed : 1 // Rounds per second
-				,
-				trail : 60 // Afterglow percentage
-				,
-				fps : 20 // Frames per second when using setTimeout() as a
-				// fallback for CSS
-				,
-				zIndex : 2e9 // The z-index (defaults to 2000000000)
-				,
-				className : 'spinner' // The CSS class to assign to the
-				// spinner
-				// , top: '50%' // Top position relative to parent
-				// , left: '50%' // Left position relative to parent
-				,
-				shadow : false // Whether to render a shadow
-				,
-				hwaccel : false
-			// Whether to use hardware acceleration
-			// , position: 'absolute' // Element positioning
-			}
+						}, 0);
+					}
+					$scope.opts = {
+						lines : 13 // The number of lines to draw
+						,
+						length : 0 // The length of each line
+						,
+						width : 14 // The line thickness
+						,
+						radius : 42 // The radius of the inner circle
+						,
+						scale : 1 // Scales overall size of the spinner
+						,
+						corners : 1 // Corner roundness (0..1)
+						,
+						color : '#000' // #rgb or #rrggbb or array of colors
+						,
+						opacity : 0.25 // Opacity of the lines
+						,
+						rotate : 0 // The rotation offset
+						,
+						direction : 1 // 1: clockwise, -1: counterclockwise
+						,
+						speed : 1 // Rounds per second
+						,
+						trail : 60 // Afterglow percentage
+						,
+						fps : 20 // Frames per second when using setTimeout()
+						// as a
+						// fallback for CSS
+						,
+						zIndex : 2e9 // The z-index (defaults to 2000000000)
+						,
+						className : 'spinner' // The CSS class to assign to
+						// the
+						// spinner
+						// , top: '50%' // Top position relative to parent
+						// , left: '50%' // Left position relative to parent
+						,
+						shadow : false // Whether to render a shadow
+						,
+						hwaccel : false
+					// Whether to use hardware acceleration
+					// , position: 'absolute' // Element positioning
+					}
 
-		});
+				} ]);
--- mysavingsweb/src/main/webapp/js/services/fdCRUDservice.js
+++ mysavingsweb/src/main/webapp/js/services/fdCRUDservice.js
@@ -1,70 +1,36 @@
-var fdCRUDservice  = angular.module("fdCRUDservice", ["ngResource"])
-.factory("fdCRUDservice", function ($resource)
-{
-  // Construct a resource object that can
-  // interact with the RESTful API of the server.
-  var resource = $resource("people/:operation/:id",
-    {
-      id: 0
-    },
-    {
-      // A custom method to update the picture of the person
-      updatePicture: {
-        method: "PUT",
-        isArray: false
-      }
-    }
-  );
 
-  // Custom function to retrieve a person by ID
-  resource.retrievePerson = function (personId) {
-    return this.get(
-      {
-        operation: "retrieve",
-        id: personId
-      });
-  };
+angular.module('app.services').factory('fdCRUDService',['$http',function($http){
+	return {
+		list : function(getFixedDepositesURL) {
+			console.log('loading Fixed Deposites ' + getFixedDepositesURL);
+			$http.get(getFixedDepositesURL)
+			.success(function(data) {
+				console.log('Fiexd Deposite data from Server :-');
+				console.log(JSON.stringify(data));
+				
+			})
+		}
+	}
+}]);
 
-  // Custom function to retrieve some people by IDs
-  resource.retrievePeople = function (peopleIdsArray) {
-    return this.query(
-      {
-        operation: "retrievearray",
-        "idsArray[]": peopleIdsArray
-      });
-  };
-
-  // Custom function to save a person object
-  resource.storePerson = function (person, picture) {
-    return this.save(
-      {
-        operation: "store",
-        firstName: person.firstName,
-        lastName: person.lastName
-      },
-      picture
-    );
-  };
-
-  // Custom function to delete a person object by ID
-  resource.erasePerson = function (personId) {
-    return this.delete(
-      {
-        operation: "erase",
-        id: personId
-      });
-  };
-
-  // Custom function to update the picture of a person
-  resource.updatePersonPicture = function (personId, picture) {
-    return this.updatePicture(
-      {
-        operation: "updatepicture",
-        id: personId
-      },
-      picture
-    );
-  };
-
-  return resource;
-})
+/*
+ * var fdCRUDservice = angular.module("fdCRUDservice", ["ngResource"])
+ * .factory("fdCRUDservice", function ($resource) { // Construct a resource
+ * object that can // interact with the RESTful API of the server. var resource =
+ * $resource("people/:operation/:id", { id: 0 }, { // A custom method to update
+ * the picture of the person updatePicture: { method: "PUT", isArray: false } } ); //
+ * Custom function to retrieve a person by ID resource.retrievePerson = function
+ * (personId) { return this.get( { operation: "retrieve", id: personId }); }; //
+ * Custom function to retrieve some people by IDs resource.retrievePeople =
+ * function (peopleIdsArray) { return this.query( { operation: "retrievearray",
+ * "idsArray[]": peopleIdsArray }); }; // Custom function to save a person
+ * object resource.storePerson = function (person, picture) { return this.save( {
+ * operation: "store", firstName: person.firstName, lastName: person.lastName },
+ * picture ); }; // Custom function to delete a person object by ID
+ * resource.erasePerson = function (personId) { return this.delete( { operation:
+ * "erase", id: personId }); }; // Custom function to update the picture of a
+ * person resource.updatePersonPicture = function (personId, picture) { return
+ * this.updatePicture( { operation: "updatepicture", id: personId }, picture ); };
+ * 
+ * return resource; })
+ */
